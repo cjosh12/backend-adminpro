2181fe0ba88f69248fe7d21e7782d28404118be9
cjosh12 <carloscalderon826@gmail.com>
2023-09-19 12:54:44 -0600

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   const logger = new Logger('bootsrap')
+  app
   
   
   app.useGlobalPipes(
